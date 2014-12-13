f7d969f72292ff2599f15601b61f241b5b6ffdfe
Paul Phillips <paulphillips@pphillips.local>
2014-12-13 17:40:25 -0600

    Add haskell

--- dev_tools.rb
+++ dev_tools.rb
@@ -1,5 +1,9 @@
 dep 'dev tools' do
   requires 'nodejs.bin'
+  requires 'haskell platform'
 end
 
-dep 'coffee.npm'
+dep 'haskell platform', :template => 'installer' do
+  source 'https://www.haskell.org/platform/download/2014.2.0.0/Haskell%20Platform%202014.2.0.0%2064bit.signed.pkg' 
+  provides 'ghc', 'cabal'
+end
