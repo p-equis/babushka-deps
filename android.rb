911b505fa7449a12e19ca2a0df05541f84869b10
Paul Phillips <paul.phillips@thoughtworks.com>
2014-12-25 07:45:21 -0600

    Add android sdk

---
+++ android.rb
@@ -0,0 +1,4 @@
+dep 'android-sdk.bin' do
+  provides 'android'
+end
+
--- dev_tools.rb
+++ dev_tools.rb
@@ -1,9 +1,12 @@
 dep 'dev tools' do
   requires 'nodejs.bin'
   requires 'haskell platform'
+  requires 'android-sdk.bin'
 end
 
 dep 'haskell platform', :template => 'installer' do
   source 'https://www.haskell.org/platform/download/2014.2.0.0/Haskell%20Platform%202014.2.0.0%2064bit.signed.pkg' 
   provides 'ghc', 'cabal'
 end
+
+
