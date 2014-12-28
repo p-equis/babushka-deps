2301ddd7eba2a7530e2dad0206d9ffb8a8ba7b66
Paul Phillips <paul.phillips@thoughtworks.com>
2014-12-28 06:59:11 -0600

    Moved intellij dep to its own file

--- dev_tools.rb
+++ dev_tools.rb
@@ -7,8 +7,3 @@ dep 'dev tools' do
   requires 'vagrant.bin'
 end
 
-dep "IntelliJ IDEA 14.app" do
-  version "14.0.2"
-  source "http://download-cf.jetbrains.com/idea/ideaIC-14.0.2.dmg"
-end
-
---
+++ intellij.rb
@@ -0,0 +1,5 @@
+dep "IntelliJ IDEA 14.app" do
+  version "14.0.2"
+  source "http://download-cf.jetbrains.com/idea/ideaIC-14.0.2.dmg"
+end
+
