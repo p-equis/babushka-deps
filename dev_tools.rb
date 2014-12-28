e14baa2428748f6391358d67b8c3bc9e7158c9d2
Paul Phillips <paul.phillips@thoughtworks.com>
2014-12-28 06:54:39 -0600

    Move haskell dep to its own file

--- dev_tools.rb
+++ dev_tools.rb
@@ -7,11 +7,6 @@ dep 'dev tools' do
   requires 'vagrant.bin'
 end
 
-dep 'haskell platform', :template => 'installer' do
-  source 'https://www.haskell.org/platform/download/2014.2.0.0/Haskell%20Platform%202014.2.0.0%2064bit.signed.pkg' 
-  provides 'ghc', 'cabal'
-end
-
 dep "IntelliJ IDEA 14.app" do
   version "14.0.2"
   source "http://download-cf.jetbrains.com/idea/ideaIC-14.0.2.dmg"
