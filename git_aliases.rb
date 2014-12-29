e300777caeaef27825382cfa3311d2360a8107df
Paul Phillips <paul.phillips@thoughtworks.com>
2014-12-29 01:23:11 -0600

    Add git co alias

--- git_aliases.rb
+++ git_aliases.rb
@@ -2,6 +2,7 @@ dep 'git aliases' do
   requires 'amend.git_alias'
   requires 'st.git_alias'
   requires 'lg.git_alias'
+  requires 'co.git_alias'
 end
 
 dep 'amend.git_alias' do
@@ -16,3 +17,6 @@ dep 'lg.git_alias' do
   long_form 'log --oneline'
 end
 
+dep 'co.git_alias' do
+  long_form 'checkout'
+end
