e35e7e964680677ea1e0e53bd3104974722220b6
Paul Phillips <paul.phillips@thoughtworks.com>
2014-12-31 14:47:11 -0600

    Revert git alias changes because they broke it.
    Revert "Reduced boilerplate needed to set up a git alias. Can't seem to pass a parameter into a template-defined dep, so doing a work-around of generating the deps at runtime"
    
    This reverts commit 6d6dc71c1afd849247984de3ef81b542aefc2049.

--- git_aliases.rb
+++ git_aliases.rb
@@ -1,17 +1,22 @@
 dep 'git aliases' do
-  gitAlias('amend', long_form: 'commit --amend')
-  gitAlias('st', long_form: 'status')
-  gitAlias('lg', long_form: 'log --oneline')
-  gitAlias('co', long_form: 'checkout')
+  requires 'amend.git_alias'
+  requires 'st.git_alias'
+  requires 'lg.git_alias'
+  requires 'co.git_alias'
 end
 
-def gitAlias(short_form, long_form)
-  newDep = "#{short_form}.git_alias"
-
-  dep newDep do 
-    long_form long_form 
-  end
-
-  requires newDep
+dep 'amend.git_alias' do
+  long_form 'commit --amend'
 end
 
+dep 'st.git_alias' do
+  long_form 'status'
+end
+
+dep 'lg.git_alias' do
+  long_form 'log --oneline'
+end
+
+dep 'co.git_alias' do
+  long_form 'checkout'
+end
