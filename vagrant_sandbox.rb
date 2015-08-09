a7a1cffcd162393c24182df2242403e5c10e0824
Paul Phillips <paul.phillips@thoughtworks.com>
2015-08-09 18:37:28 -0500

    Used the wrong plugin name for sandbox - it's actually installed by "sahara"

--- vagrant_sandbox.rb
+++ vagrant_sandbox.rb
@@ -2,10 +2,10 @@ dep 'vagrant sandbox' do
   requires 'vagrant.bin'
 
   met? {
-    shell?("vagrant plugin list | grep sandbox")
+    shell?("vagrant plugin list | grep sahara")
   }
   meet {
-    log "installing vagrant sandbox plugin"
-    shell! "vagrant plugin install sandbox"
+    log "installing vagrant sahara plugin"
+    shell! "vagrant plugin install sahara"
   }
 end
