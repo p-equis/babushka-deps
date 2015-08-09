50246c807ca10b2fc53f418d64f8bc44beeb4e95
Paul Phillips <paul.phillips@thoughtworks.com>
2015-08-09 14:11:50 -0500

    Add "git ci" alias to do incremental amends without having to change the message.

--- git_aliases.rb
+++ git_aliases.rb
@@ -3,6 +3,7 @@ dep 'git aliases' do
   requires 'st.git_alias'
   requires 'lg.git_alias'
   requires 'co.git_alias'
+  requires 'ci.git_alias' # commit incremental change
 end
 
 dep 'amend.git_alias' do
@@ -19,4 +20,8 @@ end
 
 dep 'co.git_alias' do
   long_form 'checkout'
+end
+
+dep 'ci.git_alias' do
+  long_form 'commit --amend --reuse-message=HEAD'
 end
