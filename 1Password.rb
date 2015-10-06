6e68bb54fd9bc678ddcdb5c21077b624857663d7
Paul Phillips <paul.phillips@thoughtworks.com>
2015-10-06 13:28:42 -0500

    Add 1Password dep

---
+++ 1Password.rb
@@ -0,0 +1,3 @@
+dep '1Password.app' do
+  source 'http://i.agilebits.com/dist/1P/mac4/1Password-5.3.2.zip'
+end
--- workstation.rb
+++ workstation.rb
@@ -1,5 +1,6 @@
 dep 'workstation' do
   requires 'chrome'
+  requires '1Password.app'
   requires 'dev tools'
   requires 'Sublime Text.app'
   requires 'walm:Spotify.app'
