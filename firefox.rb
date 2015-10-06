2100675af459133a244360e80e1acf16d9f189f1
Paul Phillips <paul.phillips@thoughtworks.com>
2015-10-06 13:28:43 -0500

    Add firefox browser

---
+++ firefox.rb
@@ -0,0 +1,3 @@
+dep 'Firefox.app' do
+	source 'https://download.mozilla.org/?product=firefox-41.0.1-SSL&os=osx&lang=en-US'
+end
--- workstation.rb
+++ workstation.rb
@@ -1,5 +1,6 @@
 dep 'workstation' do
   requires 'chrome'
+  requires 'Firefox.app'
   requires '1Password.app'
   requires 'dev tools'
   requires 'Sublime Text'
