6024f894f345079d2ba6aafe543f3cf6029f8bce
Paul Phillips <paul.phillips@thoughtworks.com>
2015-10-06 13:28:42 -0500

    Fix broken intellij link; upgrade which version of intellij community is installed

--- intellij.rb
+++ intellij.rb
@@ -1,5 +1,8 @@
 dep "IntelliJ IDEA 14.app" do
   version "14.0.2"
-  source "http://download-cf.jetbrains.com/idea/ideaIC-14.0.2.dmg"
+  source "https://download.jetbrains.com/idea/ideaIC-14.1.4.dmg"
 end
 
+dep '1Password.app' do
+  source 'http://i.agilebits.com/dist/1P/mac4/1Password-4.4.3.zip'
+end
