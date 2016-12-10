9c46a7d0e07c4dbd373d7234be49df54de82d0a1
paulphillips <f@f.com>
2016-12-10 10:42:38 -0600

    Add vs code c# extensions

--- dev_tools/visual_studio_code.rb
+++ dev_tools/visual_studio_code.rb
@@ -1,9 +1,19 @@
 dep 'visual studio code' do
   requires 'Visual Studio Code.app'
+  requires 'vs code c#'
 end
 
 dep 'Visual Studio Code.app' do
   meet {
     shell('brew cask install visual-studio-code')
+  }
+end
+
+dep 'vs code c#' do
+  met? {
+    shell('code --list-extensions | grep "ms-vscode.csharp"') 
+  }
+  meet {
+    shell('code --install-extension ms-vscode.csharp')
   }
 end
